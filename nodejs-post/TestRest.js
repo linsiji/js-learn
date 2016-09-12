66131f941ab6deed84e46366961b771dc68c2c6b
linsiji <lin_jianduan@163.com>
2016-09-12 18:19:31 +0800

    Revert "修复304"
    This reverts commit fddc1d9665ef7a3b08628575360fbe9ee0abb7bb.

--- nodejs-post/TestRest.js
+++ nodejs-post/TestRest.js
@@ -9,15 +9,15 @@ var equal = require('assert').equal;
 var lingdoong = require('./plugin/lingdoong');
  
 var username = "13022996666";
-var params = {api_serialNo:"1608011235448789212",api_orderId:"2016052710200",api_token:"Y7MyJsSuSwVxqGh49uAHIkV9Zk/qqiUtMfwI+X0Oe8KFDDKjp0oL3A=="};
+var params = {api_serialNo:"1608011235448789212",api_userName:"张老师",api_telephone:"13022995566",api_idCard:"610302198532615478",api_bankCard:"6222600810011012345",api_creditLvl:"3"};
 
 var post_data = lingdoong.setPost(username, params);
 console.log("test",post_data);
  
 var options = {
-    host: 'file.wodiantong.com',
-    port: 80,
-    path: '/woEasy_file/api/baifm/getOrders',
+    host: '127.0.0.1',
+    port: 8080,
+    path: '/test/api/baifm/register',
     method: 'POST',
     headers: {
     	'Content-Type': 'application/x-www-form-urlencoded',
